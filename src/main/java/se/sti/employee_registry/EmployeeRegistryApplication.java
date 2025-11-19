23b293fd07f9414ab6f6ec93a5a324f765f82f95
Martin Eriksson <martineriksson999@gmail.com>
2025-11-19 09:50:38 +0100

    Initial commit

---
+++ src/main/java/se/sti/employee_registry/EmployeeRegistryApplication.java
@@ -0,0 +1,13 @@
+package se.sti.employee_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistryApplication.class, args);
+	}
+
+}
